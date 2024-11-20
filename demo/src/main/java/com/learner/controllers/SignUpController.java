65e977f0dfe765b168b19f6307290e24c6797237
trbraley03 <trbraley03@gmail.com>
2024-11-20 00:16:56 -0500

    added in skeleton for signupcontroller.java

---
+++ demo/src/main/java/com/learner/controllers/SignUpController.java
@@ -0,0 +1,28 @@
+package com.learner.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class SignUpController {
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private Button enterButton;
+
+    @FXML
+    private TextField enterDisplayName;
+
+    @FXML
+    private TextField enterEmail;
+
+    @FXML
+    private PasswordField enterPassword;
+
+    @FXML
+    private TextField enterUsername;
+
+}
