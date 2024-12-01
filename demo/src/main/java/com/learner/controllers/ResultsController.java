252b1020b1dabaee8699c4a8ac2785e98fbb9783
mauriciono <mthooks@email.sc.edu>
2024-12-01 15:08:56 -0500

    added Results and TF pages

---
+++ demo/src/main/java/com/learner/controllers/ResultsController.java
@@ -0,0 +1,20 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+
+public class ResultsController {
+    @FXML
+    private Button HomeButton;
+
+    @FXML
+    private Button NewGameButton;
+
+    @FXML
+    private Button PlayAgainButton;
+
+    @FXML
+    void goToMain(ActionEvent event) {
+
+    }
+}
