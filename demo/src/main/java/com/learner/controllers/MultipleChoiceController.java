3ae1c1d17c7edd408938cc1a8bf02d740050cca3
mauriciono <mthooks@email.sc.edu>
2024-12-01 19:25:17 -0500

    added multiplechoice page

---
+++ demo/src/main/java/com/learner/controllers/MultipleChoiceController.java
@@ -0,0 +1,19 @@
+package com.learner.controllers;
+
+public class MultipleChoiceController {
+    @FXML
+    private Button homeButton;
+
+    @FXML
+    private Button questionButton;
+
+    @FXML
+    void goToHome(ActionEvent event) {
+
+    }
+
+    @FXML
+    void goToResults(ActionEvent event) {
+
+    }
+}
