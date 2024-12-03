a3e2bd54fe3c000682c5d1f58d668e3479e7afaa
SMDindl <SDINDL@email.sc.edu>
2024-12-03 11:58:19 -0500

    Updated category class

--- demo/src/main/java/com/learner/model/innerdata/GameCategory.java
+++ demo/src/main/java/com/learner/model/innerdata/GameCategory.java
@@ -5,23 +5,37 @@ package com.learner.model.innerdata;
  */
 public enum GameCategory {
 
-    WORD("Word Game"), 
-    STORY("Story Game"), 
-    ALPHABET("Alphabet Game");
+    WORD("Word Wonders", "Build Language Foundation"), 
+    ALPHABET("Letter Launch", "Explore the Alphabet"),
+    CULTURE("Cultural Connect", "Master Real-Life Situations"),
+    STORY("Stories", "Improve Reading Proficiency");
 
-    private final String category;
+    private final String categoryName;
+    private final String description;
 
     /**
-     * Constructor to initialize the display name
-     * @param displayName
+     * Constructor for GameCategory
+     * @param categoryName
+     * @param description
      */
-    GameCategory(String category) {
-        this.category = category;
+    GameCategory(String categoryName, String description) {
+        this.categoryName = categoryName;
+        this.description = description;
     }
 
-    // Getter for the string representation
+    // Getter for the enum instance of the category
+    public GameCategory getGameCategory() {
+        return this;
+    }
+
+    // Getter for the category name
     public String getCategory() {
-        return category;
+        return categoryName;
+    }
+
+    // Getter for the description
+    public String getDescription() {
+        return description;
     }
 
 }
