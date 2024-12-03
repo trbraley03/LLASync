f401c96abec267902c8f2f1ef8aedbc9d6757dba
SMDindl <SDINDL@email.sc.edu>
2024-12-02 19:15:44 -0500

    Began creating screens to hold game content

---
+++ demo/src/main/java/com/learner/controllers/GameDefaultContentController.java
@@ -0,0 +1,32 @@
+package com.learner.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class GameDefaultContentController {
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private Label gameContentText;
+
+    @FXML
+    private Button nextButton;
+
+    @FXML
+    private Label title;
+
+    @FXML
+    void goBackToPreviousGameScreen(ActionEvent event) {
+
+    }
+
+    @FXML
+    void goToNextGameScreen(ActionEvent event) {
+
+    }
+
+}
