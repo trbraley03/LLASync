c669a19131d839d53f369875cf30a318c73b52e9
trbraley03 <trbraley03@gmail.com>
2024-12-05 20:37:00 -0500

    Added updates to PickLanguageController, ProgressTrackerController, and added language PNGs

--- demo/src/main/java/com/learner/controllers/ProgressTrackerController.java
+++ demo/src/main/java/com/learner/controllers/ProgressTrackerController.java
@@ -1,8 +1,11 @@
 package com.learner.controllers;
 
 import java.io.IOException;
+import java.util.UUID;
 
 import com.learner.game.App;
+import com.learner.model.Facade;
+import com.learner.model.User.ProgressTracker;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -17,12 +20,33 @@ public class ProgressTrackerController {
     @FXML
     private ProgressBar gamesCompletedBar;
 
+    private static final int TOTAL_GAMES = 20;
+    private ProgressTracker progressTracker;
+
+    public ProgressTrackerController() {
+        // Access current user's progress tracker
+        Facade facade = Facade.getInstance();
+        progressTracker = facade.getCurrentUser().getProgressTracker(facade.getCurrentLanguage().getUUID());
+    }
+
     @FXML
-    private ProgressBar gamesTotalBar;
+    public void initialize() {
+        updateProgressBar();
+    }
 
     @FXML
     void goToHome(ActionEvent event) throws IOException {
         App.setRoot("home");
     }
 
+    private void updateProgressBar() {
+        int completedGames = progressTracker.getTotalCompletedGames();
+        double progress = (double) completedGames / TOTAL_GAMES;
+        gamesCompletedBar.setProgress(progress);
+    }
+
+    public void onGameCompleted(UUID gameUUID) {
+        progressTracker.addCompletedGame(gameUUID);
+        updateProgressBar();
+    }
 }
