a746b53aea7bb4c556274826e7852346b8b32268
SMDindl <SDINDL@email.sc.edu>
2024-12-06 09:07:58 -0500

    Updated difficulty select, css styling for all buttons, made languages capitalized in json

--- demo/src/main/java/com/learner/controllers/DifficultySelectController.java
+++ demo/src/main/java/com/learner/controllers/DifficultySelectController.java
@@ -1,17 +1,29 @@
 package com.learner.controllers;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import com.learner.game.App;
 import com.learner.model.Facade;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 
-public class DifficultySelectController {
+public class DifficultySelectController implements Initializable {
 
     private final Facade facade = Facade.getInstance();
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private Label currentLanguageNameDisplay;
     
     @FXML
     private ImageView easyButton;
@@ -41,6 +53,20 @@ public class DifficultySelectController {
         facade.selectLanguage("filipino");
         facade.selectDifficulty("medium");
         App.setRoot("gameSelect");
+    }
+
+    @FXML
+    private void goBackToLangSelect(ActionEvent event) {
+        try {
+            App.setRoot("pickLanguage");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        currentLanguageNameDisplay.setText(facade.getCurrentLanguage().getLanguageName());
     }
 
 }
