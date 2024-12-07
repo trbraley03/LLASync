ae4f63c18bdc8b4920cfd0e6b14da8352f5239e5
SMDindl <SDINDL@email.sc.edu>
2024-12-07 02:59:14 -0500

    Added images, better game select

--- demo/src/main/java/com/learner/controllers/GameSelectController.java
+++ demo/src/main/java/com/learner/controllers/GameSelectController.java
@@ -1,8 +1,9 @@
 package com.learner.controllers;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.ResourceBundle;
 import java.util.UUID;
 
@@ -10,7 +11,9 @@ import com.learner.game.App;
 import com.learner.model.Difficulty;
 import com.learner.model.Facade;
 import com.learner.model.Game;
+import com.learner.model.GameManager;
 import com.learner.model.Language;
+import com.learner.model.innerdata.GameCategory;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -19,7 +22,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
 
 public class GameSelectController implements Initializable {
 
@@ -28,66 +32,108 @@ public class GameSelectController implements Initializable {
     private final Difficulty currentDifficulty = facade.getCurrentDifficulty();
 
     @FXML
-    private VBox mainVbox;
+    private Button backButton;
 
     @FXML
-    private Button backButton;
+    private Text category1;
+
+    @FXML
+    private Text category2;
+
+    @FXML
+    private Text category3;
+
+    @FXML
+    private HBox hbox1;
+
+    @FXML
+    private HBox hbox2;
+
+    @FXML
+    private HBox hbox3;
 
     @FXML
     private Label title;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        for (Game game : facade.getAvailableGames()) {
-            Button gameButton = new Button();
-            String gameTitle = game.getGameTitle();
-            UUID gameUUID = game.getUUID();
-            gameButton.setUserData(gameUUID);
-            gameButton.setMaxWidth(98);
-            gameButton.setMaxHeight(85);
-            gameButton.setPrefWidth(98);
-            gameButton.setPrefHeight(85);
-            gameButton.setMinWidth(100);
-            gameButton.setMinHeight(80);
+        HashMap<GameCategory, ArrayList<Game>> gamesByCategory = GameManager.getInstance().getGamesByCategory(currentLanguage.getUUID(), currentDifficulty);
 
-            // Check if the image file exists
-            String imagePath = "/com/learner/game/game-select-icons/" + gameTitle + ".png";
-            URL imageUrl = getClass().getResource(imagePath);
-            if (imageUrl != null) {
-                // Set the button's graphic to the image
-                ImageView imageView = new ImageView(new Image(imageUrl.toString()));
-                imageView.setFitWidth(100);  // Adjust the width as needed
-                imageView.setFitHeight(100); // Adjust the height as needed
-                imageView.setPreserveRatio(true);
-                gameButton.setGraphic(imageView);
-            } else {
-                // Fallback to setting the button's text to the game name
-                gameButton.setText(gameTitle);
-                System.out.println("image file not found:" + gameTitle);
-            }
+        for (GameCategory category : gamesByCategory.keySet()) {
+            ArrayList<Game> games = gamesByCategory.get(category);
+            HBox targetHBox = getTargetHBox(category);
 
-            // Add the button to your VBox or other container
-            mainVbox.getChildren().add(gameButton);
+            for (Game game : games) {
+                Button gameButton = new Button();
+                String gameTitle = game.getGameTitle();
+                UUID gameUUID = game.getUUID();
+                gameButton.setUserData(gameUUID);
+                gameButton.setMaxWidth(98);
+                gameButton.setMaxHeight(85);
+                gameButton.setPrefWidth(98);
+                gameButton.setPrefHeight(85);
+                gameButton.setMinWidth(100);
+                gameButton.setMinHeight(80);
 
-            // Set up the event handler
-            gameButton.setOnAction(event -> {
-                UUID clickedUUID = (UUID) gameButton.getUserData();
-                facade.selectGame(clickedUUID);
-                try {
-                    goToGameIntroduction(event);
-                } catch (IOException e) {
-                    e.printStackTrace();
+                // Check if the image file exists
+                String imagePath = "/com/learner/game/game-select-icons/" + gameTitle + ".png";
+                URL imageUrl = getClass().getResource(imagePath);
+                if (imageUrl != null) {
+                    // Set the button's graphic to the image
+                    ImageView imageView = new ImageView(new Image(imageUrl.toString()));
+                    imageView.setFitWidth(100);  // Adjust the width as needed
+                    imageView.setFitHeight(100); // Adjust the height as needed
+                    imageView.setPreserveRatio(true);
+                    gameButton.setGraphic(imageView);
+                } else {
+                    // Fallback to setting the button's text to the game name
+                    gameButton.setText(gameTitle);
+                    System.out.println("image file not found:" + gameTitle);
                 }
-            });
+
+                // Add the button to the appropriate HBox
+                targetHBox.getChildren().add(gameButton);
+
+                // Set up the event handler
+                gameButton.setOnAction(event -> {
+                    UUID clickedUUID = (UUID) gameButton.getUserData();
+                    facade.selectGame(clickedUUID);
+                    try {
+                        goToGameIntroduction(event);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+            }
         }
+
         title.setText(currentDifficulty.getLabel() + " " + currentLanguage.getLanguageName() + " Games");
     }
 
+    private HBox getTargetHBox(GameCategory category) {
+        switch (category) {
+            case WORD:
+                category1.setText(category.getCategory() + ": " + category.getDescription());
+                return hbox1;
+            case ALPHABET:
+            case CULTURE:
+                category2.setText(category.getCategory() + ": " + category.getDescription());
+                return hbox2;
+            case STORY:
+            category3.setText(category.getCategory() + ": " + category.getDescription());
+                return hbox3;
+            default:
+                return new HBox(); // Default case, should not happen
+        }
+    }
+
     @FXML
-    private void goToSetLangAndDiff(ActionEvent event) throws IOException {
-        Difficulty reset = null;
-        facade.selectDifficulty(reset);
-        App.setRoot("setDifficulty");   
+    private void goToLanguageSelect(ActionEvent event) {
+        try {
+            App.setRoot("setDifficulty");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
@@ -95,4 +141,3 @@ public class GameSelectController implements Initializable {
         App.setRoot("gameIntroScreen");
     }
 }
-
